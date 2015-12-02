e5a6b674f09c534abb7ef66b51bba60befad5313
kennmyers <kennmyers@users.noreply.github.com>
2015-12-01 20:20:51 -0500

    inputed code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+import csv
+from advanced_python_regex import Faculty
+
+emails = Faculty("faculty.csv").get_emails()
+#print(emails)
+with open("emails.csv", 'w') as file:
+    writer = csv.writer(file, delimiter = "\n")
+    writer.writerows([emails])
